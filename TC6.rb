204f9066f1605255c051a168d8ddffb91e862bce
thangato997 <69310660+thangato997@users.noreply.github.com>
2020-09-22 15:27:18 +0700

    7 testcases

--- TC6.rb
+++ TC6.rb
@@ -4,8 +4,8 @@ driver = Selenium::WebDriver.for :firefox
 wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 driver.get 'https://www.google.com/'
 driver.manage.window.maximize
-driver.find_element(:name, 'q').send_keys('iTMS Coaching')
-driver.find_element(:name, 'q').send_keys(:enter)
+driver.find_element(:css, '[name="q"]').send_keys('iTMS Coaching')
+driver.find_element(:css, '[name="q"]').send_keys(:enter)
 wait.until { driver.find_element(:xpath, "//a[@id='logo']//img").displayed? }
 current_title = driver.title
 puts current_title
--- TC7.rb
+++ TC7.rb
@@ -6,18 +6,18 @@ driver = Selenium::WebDriver.for :firefox
 driver.get 'https://itmscoaching.herokuapp.com/form'
 # Resize window
 driver.manage.window.maximize
-driver.find_element(:xpath, "//input[@id='first-name']").send_keys('iTMS')
-driver.find_element(:xpath, "//input[@id='last-name']").send_keys('Coaching')
-driver.find_element(:xpath, "//input[@id='job-title']").send_keys('QA')
+driver.find_element(:css, '#first-name').send_keys('iTMS')
+driver.find_element(:css, '#last-name').send_keys('Coaching')
+driver.find_element(:css, '#job-title').send_keys('QA')
 # Check the radio button is checked or not
-radio_button = driver.find_element(:xpath, "//input[@id='radio-button-2']")
+radio_button = driver.find_element(:css, '#radio-button-2')
 if (radio_button.selected?)
     pass
 else
     radio_button.click
 end
 # Check the checkbox is checked or not
-checkbox = driver.find_element(:xpath, "//input[@id='checkbox-1']")
+checkbox = driver.find_element(:css, '#checkbox-1')
 if (checkbox.selected?)
     pass
 else
@@ -29,7 +29,7 @@ select_list = driver.find_element(:xpath, "//select[@id='select-menu']") # Get d
 dropdown = Selenium::WebDriver::Support::Select.new(select_list) # Create dropdown list
 dropdown.select_by(:text, text) # Select value by text from dropdown list
 
-driver.find_element(:xpath, "//input[@id='datepicker']").send_keys('27/10/2025')
+driver.find_element(:css, '#datepicker').send_keys('27/10/2025')
 driver.find_element(:xpath, "//a[@class='btn btn-lg btn-primary']").click
 sleep 2
 driver.close
