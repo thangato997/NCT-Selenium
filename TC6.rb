810737948001903b3d10e74fec9d855b8f858876
thangato997 <69310660+thangato997@users.noreply.github.com>
2020-09-22 16:02:37 +0700

    7 testcases

--- TC6.rb
+++ TC6.rb
@@ -1,11 +1,11 @@
 require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for :firefox
-wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 driver.get 'https://www.google.com/'
 driver.manage.window.maximize
 driver.find_element(:css, '[name="q"]').send_keys('iTMS Coaching')
 driver.find_element(:css, '[name="q"]').send_keys(:enter)
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
 wait.until { driver.find_element(:xpath, "//a[@id='logo']//img").displayed? }
 current_title = driver.title
 puts current_title
