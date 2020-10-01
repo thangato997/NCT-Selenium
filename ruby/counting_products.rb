9081c19fe04b1f3fcdd5bf005039e7473a5bff98
thangato997 <thangato997@gmail.com>
2020-10-02 01:55:56 +0700

    Counting products are displayed after searching

---
+++ ruby/counting_products.rb
@@ -0,0 +1,25 @@
+require "selenium-webdriver"
+
+$value = "book"
+
+describe "Count Products that were displayed" do
+    before(:all) do
+        @driver = Selenium::WebDriver.for :chrome
+    end
+
+    before(:each) do
+        @driver.get "https://tiki.vn/"
+    end
+
+    after(:all) do
+        @driver.close
+    end
+    it "count the books were displayed" do
+        @driver.find_element(css: ".eUnWAD").send_keys $value, :enter
+        # using find_elements by css via class
+        elements = @driver.find_elements(css: ".search-a-product-item")
+        # get total of products by get size of value elements 
+        count = elements.size
+        puts ("Có " + count.to_s + " sản phầm được hiển thị")
+    end
+end
