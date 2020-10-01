f97384b77e02444ea46b990f42843852a85b4511
thangato997 <thangato997@gmail.com>
2020-10-02 02:26:07 +0700

    Counting products are displayed after searching - updated

--- ruby/counting_products.rb
+++ ruby/counting_products.rb
@@ -1,6 +1,7 @@
 require "selenium-webdriver"
 
 $value = "book"
+$products = 52
 
 describe "Count Products that were displayed" do
     before(:all) do
@@ -16,12 +17,15 @@ describe "Count Products that were displayed" do
     end
     it "count the books were displayed" do
         @driver.find_element(css: ".eUnWAD").send_keys $value, :enter
-        # use implicit_wait to wait the page is loaded
+        # use implicit_wait to wait for the page to load
         @driver.manage.timeouts.implicit_wait = 10
         # use find_elements by css via class
         elements = @driver.find_elements(css: ".search-a-product-item")
-        # get total of products by get size of value elements 
+        # get total of products by getting size of the value elements 
         count = elements.size
+        # confirm the count is correct or not
+        expect(count).to eql($products)
+        # print to the screen
         puts count.to_s + " products are counted"
     end
 end
